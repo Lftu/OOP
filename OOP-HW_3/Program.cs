172b4ae333440aacec8e0f3ec496a753ede2d827
Lftu <91163554+Lftu@users.noreply.github.com>
2022-12-26 13:14:09 +0200

    OOP
    OOP-3

---
+++ OOP-HW_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace yehor
+{
+    public partial class Form1 : Form
+    {
+
+        string num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            num = textBox1.Text;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  string message = textBox1.Text;
+            //Console.WriteLine(message);
+            MessageBox.Show("Your entered number: " + num);
+        }
+    }
+}
